2f05c38699a0db67bbf19a0006879f9f9793d99a
KylianB <kylian.balde@gmail.com>
2023-01-16 19:41:32 +0100

    rework deposit spawning method

--- Source/VHelixienGasE/HelixienGasHandler.cs
+++ Source/VHelixienGasE/HelixienGasHandler.cs
@@ -1,47 +1,80 @@
-﻿using RimWorld;
+﻿using System.Collections.Generic;
+using RimWorld;
 using Verse;
 
 namespace VHelixienGasE
 {
     public class HelixienGasHandler : MapComponent
     {
-        internal BoolGrid infiniteGasGrid = null;
+        public BoolGrid infiniteGasGrid = null;
 
-        public HelixienGasHandler(Map map) : base(map) { }
+        private int depositAmount = 0;
+
+        public HelixienGasHandler(Map map) : base(map)
+        {
+        }
 
         public override void FinalizeInit()
         {
             if (infiniteGasGrid == null && map.ParentFaction == Faction.OfPlayer)
-            {
                 infiniteGasGrid = new BoolGrid(map);
-                // Make deposit    
-                if (!CellFinderLoose.TryFindRandomNotEdgeCellWith(10, x => CanScatterAt(x, map), map, out IntVec3 result))
-                    Log.Error("Could not find a center cell for infinite helixien gas deposit");
 
-                var def = TDefOf.VHGE_Helixien;
-                foreach (var intVec3 in GridShapeMaker.IrregularLump(result, map, 15))
-                {
-                    if (CanScatterAt(intVec3, map) && !intVec3.InNoBuildEdgeArea(map))
-                    {
-                        map.deepResourceGrid.SetAt(intVec3, def, def.deepCountPerCell);
-                        infiniteGasGrid[intVec3] = true;
-                    }
-                }
-
-                Find.LetterStack.ReceiveLetter("VHGE_InfiniteDepositLabel".Translate(), "VHGE_InfiniteDeposit".Translate(), LetterDefOf.PositiveEvent, new LookTargets(result, map));
-            }
+            CreateInfiniteDeposit();
         }
 
         public override void ExposeData()
         {
             Scribe_Deep.Look(ref infiniteGasGrid, "infiniteGasGrid", null);
+            Scribe_Values.Look(ref depositAmount, "depositAmount");
+        }
+
+        private void CreateInfiniteDeposit()
+        {
+            // Make deposit
+            if (!CellFinderLoose.TryGetRandomCellWith(x => CanScatterAt(x, map), map, 10000, out IntVec3 origin))
+                Log.Error("Could not find a center cell for infinite helixien gas deposit");
+
+            var def = TDefOf.VHGE_Helixien;
+            foreach (var cell in IrregularLump(origin, 15, map))
+            {
+                if (!cell.InNoBuildEdgeArea(map) && CanScatterAt(cell, map))
+                {
+                    map.deepResourceGrid.SetAt(cell, def, def.deepCountPerCell);
+                    infiniteGasGrid[cell] = true;
+                }
+            }
+        }
+
+        private List<IntVec3> IrregularLump(IntVec3 center, int numCells, Map map)
+        {
+            var lumpCells = new List<IntVec3>();
+            var count = 0;
+
+            while (count < numCells)
+            {
+                var cell = count > 0 ? lumpCells.RandomElement() : center;
+                cell += GenAdj.CardinalDirections[Rand.RangeInclusive(0, 3)];
+
+                if (!lumpCells.Contains(cell) && CanScatterAt(cell, map))
+                {
+                    lumpCells.Add(cell);
+                    count++;
+                }
+            }
+            return lumpCells;
         }
 
         private bool CanScatterAt(IntVec3 pos, Map map)
         {
             int index = CellIndicesUtility.CellToIndex(pos, map.Size.x);
             var terrainDef = map.terrainGrid.TerrainAt(index);
-            return (terrainDef == null || !terrainDef.IsWater || terrainDef.passability != Traversability.Impassable) && terrainDef.affordances.Contains(ThingDefOf.DeepDrill.terrainAffordanceNeeded) && !map.deepResourceGrid.GetCellBool(index);
+
+            return pos.InBounds(map)
+                   && !pos.InNoBuildEdgeArea(map)
+                   && (terrainDef == null || !terrainDef.IsWater || terrainDef.passability != Traversability.Impassable)
+                   && terrainDef.affordances.Contains(TerrainAffordanceDefOf.Medium)
+                   && !map.deepResourceGrid.GetCellBool(index)
+                   && !pos.Impassable(map);
         }
     }
 }
