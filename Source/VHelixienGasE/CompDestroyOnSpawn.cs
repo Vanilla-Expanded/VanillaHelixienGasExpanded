3784edbaa2ce282aee56e1087b118022f7c140f8
KylianB <kylian.balde@gmail.com>
2023-07-08 14:52:58 +0200

    CompDestroyOnSpawn

---
+++ Source/VHelixienGasE/CompDestroyOnSpawn.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PipeSystem;
+using UnityEngine;
+using Verse;
+
+namespace VHelixienGasE
+{
+    public class CompProperties_CompDestroyOnSpawn : CompProperties
+    {
+        public CompProperties_CompDestroyOnSpawn()
+        {
+            compClass = typeof(CompDestroyOnSpawn);
+        }
+    }
+
+    internal class CompDestroyOnSpawn : ThingComp
+    {
+        public override void PostSpawnSetup(bool respawningAfterLoad)
+        {
+            base.PostSpawnSetup(respawningAfterLoad);
+            parent.DeSpawn(DestroyMode.Vanish);
+        }
+    }
+}
